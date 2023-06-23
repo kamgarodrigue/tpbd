a54a718c99a7eb253d3499b07563d721ab91ba09
kamga <kamgarodrigue54@gmail.com>
2023-06-23 13:55:35 +0200

    front detail

--- src/routes/voiture/detail.js
+++ src/routes/voiture/detail.js
@@ -1,29 +1,38 @@
 const {voitureTable, categoriTable, typeVoitureTable, consoTable, marqueTable}= require("../../db/sequelize");
-const {Op,Sequelize} = require('sequelize');
+const {Op,Sequelize, json} = require('sequelize');
 
 
 module.exports = (app)=>{
-  app.get("/api/voiture/detail/", (req ,res)=>{
+  app.get("/api/voiture/detail/:id", (req ,res)=>{
     if (!req.session.user) {
 
       return res.redirect('/');
     }
   
     voitureTable.findAll({
-        include:[categoriTable,typeVoitureTable,consoTable,marqueTable]
+        include:[categoriTable,typeVoitureTable,consoTable,marqueTable],
+        
+    },
+    {where:{id:req.params.id}
+        
     })
-      .then(async voiture =>{
+      .then(async voitures =>{
         const [categori, typeVoiture, consomation,marque] = await Promise.all([
           categoriTable.findAll(),
           typeVoitureTable.findAll(),consoTable.findAll(),marqueTable.findAll()
       ]);
         const message ="La liste a bien ete recuperer!"
+     let  voiture= voitures.filter(function(item) {
+      
+          return item.id == req.params.id})[0].dataValues;
+          console.log(voiture.category.dataValues.nom);
      let  data={user:req.session.user,voiture,categori, typeVoiture, consomation,marque}
+   //  console.log(req.params.id)
      console.log(data)
         res.render('detail',data);
        // res.status(200).json({message,data: voiture})
        
-
+ 
       })
       .catch(err =>{
         console.log(err)
